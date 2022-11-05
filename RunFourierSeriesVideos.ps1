d070ad5719c67ae1df24cb156ced6ec355db014e
Nick Skriloff <skriloffn@darden.virginia.edu>
2022-11-05 13:14:49 -0400

    update RunFourier with help from Alex

--- RunFourierSeriesVideos.ps1
+++ RunFourierSeriesVideos.ps1
@@ -1,11 +1,29 @@
 #pull the image with the dependencies for
 # Install FFmpeg, Cairo, LaTeX and sox. 
-docker pull eulertour/manim:latest
-docker run -it eulertour/manim:latest
+
+#docker pull eulertour/manim:latest
+#docker run -it eulertour/manim:latest
 # At command prompt
-cd manim
+#$cd manim
 
+$env:INPUT_PATH="C:\devHelp\manim-3b1b\"
+$env:OUTPUT_PATH="C:\devHelp\manim-3b1b\"
 
-INPUT_PATH=/path/to/dir/containing/source/code \
-OUTPUT_PATH=/path/to/output/ \
-docker-compose run manim example_scenes.py SquareToCircle -l
+#winget install --silent --name "mpv (Unofficial)"
+# which mpv # will produce
+#C:\Users\SkriloffN\AppData\Local\Microsoft\WindowsApps\mpv.EXE
+#New-Item "C:\Users\SkriloffN\AppData\Local\Microsoft\WindowsApps\mpv.conf"
+#Write-Output  “keep-open=yes” > "C:\Users\SkriloffN\AppData\Local\Microsoft\WindowsApps\input.conf"
+#type "C:\Users\SkriloffN\AppData\Local\Microsoft\WindowsApps\mpv.conf"
+# empv --keep-open=yes  .\videos\example_scenes\480p15\SquareToCircle.mp4  
+# $env:APPDATA
+# mkdir $env:APPDATA\mpv
+# mv “C:\….\input.conf” $env:APPDATA\mpv\mpv.conf
+
+#docker-compose run manim example_scenes.py SquareToCircle -l
+
+#docker-compose run manim <file-name>.py <Scene> -l
+# example:
+#docker-compose run manim example_scenes.py SquareToCircle -l
+
+docker-compose run manim fourierFromAlex.py -l
